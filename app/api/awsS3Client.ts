6f711c4345d4d3c54ff75e2059468aee9d3a8866
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-01 09:06:35 -0800

    change bucket name

--- app/api/awsS3Client.ts
+++ app/api/awsS3Client.ts
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-const bucketName = "chasing-sunrise" ;
+const bucketName = "chasing-sunrise-pictures";
 
 const s3Client = new S3Client({
   credentials: {
