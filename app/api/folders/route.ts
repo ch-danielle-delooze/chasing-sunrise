1fa6b8cac78f1b578040c223ae607d3702d433fb
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-02 11:24:08 -0800

    add two functions to covert ListObjectCommand response to a folder directory for front-end use

--- app/api/folders/route.ts
+++ app/api/folders/route.ts
@@ -1,14 +1,61 @@
-import { ListObjectsCommand } from "@aws-sdk/client-s3"
+import { ChecksumType, ListObjectsCommand, ListObjectsCommandOutput } from "@aws-sdk/client-s3"
+import {} from "@aws-sdk/types"
 import { NextResponse } from "next/server"
 
 import { s3Client, bucketName } from "../awsS3Client"
+
+type Folder = {
+  name: string
+  objects: string[]
+  subFolders: Folder[]
+}
+
+const findOrCreateFolder = (
+  parentSubFolders: Folder[],
+  folderName: string
+): Folder => {
+  const folder =
+    parentSubFolders?.find((subFolder) => subFolder.name === folderName) ??
+    (() => {
+      const newFolder: Folder = { name: folderName, subFolders: [], objects: [] };
+      parentSubFolders.push(newFolder);
+      return newFolder;
+    })();
+
+  return folder;
+};
+
+const convertListObjectResponseToFolderJson = (
+  output: ListObjectsCommandOutput
+): Folder[] => {
+  const result: Folder[] = [];
+
+  output.Contents?.forEach(({ Key, ChecksumType }) => {
+    if (!Key || !ChecksumType) return;
+
+    const folders = Key.split("/");
+    let previousFolder: Folder | undefined = undefined;
+    folders.forEach((folderName, index) => {
+      const isLast = index === folders.length - 1;
+      if (isLast) {
+        previousFolder?.objects.push(Key);
+      } else {
+        const folder = findOrCreateFolder(previousFolder?.subFolders ?? result, folderName);
+        previousFolder = folder;
+      }
+    });
+  });
+
+  return result;
+}
 
 const GET = async () => {
   const command = new ListObjectsCommand({
     Bucket: bucketName,
   })
   const response = await s3Client.send(command)
-  return NextResponse.json(response)
+  const folders = convertListObjectResponseToFolderJson(response)
+  return NextResponse.json(folders)
 }
 
 export { GET };
