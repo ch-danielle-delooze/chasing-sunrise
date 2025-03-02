5fb7608f1030c74ec840f1047b6dc7e21bdebdbd
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-02 12:23:09 -0800

    cache folder response for duration of user session

--- app/requests/folders/index.ts
+++ app/requests/folders/index.ts
@@ -4,7 +4,10 @@ import { Folder } from "@/types";
 
 export function useGetFolders(): SWRResponse<Folder[], Error> {
   const url = "/api/folders";
-  const resp: SWRResponse<Folder[], Error> = useSWR(url);
+  const resp: SWRResponse<Folder[], Error> = useSWR(url, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 
   if (resp.error) {
     console.error("Error fetching folders:", resp.error);
