dbbc55fe865c4a9984c30d1ec8d95c1e54b1c3dc
Danielle DeLooze <118206403+ch-danielle-delooze@users.noreply.github.com>
2025-03-09 14:39:13 -0700

    Spacing on image page + image download (#6)
    * download image on click
    
    * change hover effect
    
    * fix mobile spacing

--- components/imagePage/Image.tsx
+++ components/imagePage/Image.tsx
@@ -10,7 +10,32 @@ interface ImageProps {
 const ImageDisplay = ({ imagePath }: ImageProps) => {
   const { data } = useGetImage({ key: imagePath });
 
-  return <Image alt="Image" src={data ?? undefined} />;
+  const downloadImage = async () => {
+    if (!data) return;
+    const response = await fetch(data, {
+      method: "GET",
+      mode: "cors",
+      cache: "no-store",
+    });
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+
+    link.href = url;
+    link.download = imagePath.split("/")[1];
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Image
+      alt="Image"
+      className="md:m-6 my-2 hover:scale-105 cursor-pointer"
+      src={data ?? undefined}
+      onClick={downloadImage}
+    />
+  );
 };
 
 export default ImageDisplay;
