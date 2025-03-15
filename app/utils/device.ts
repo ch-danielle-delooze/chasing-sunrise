3e4046eff679b3ec68a55aefe2ec1aaea5493427
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-14 21:09:21 -0700

    fix invalid reference

--- app/utils/device.ts
+++ app/utils/device.ts
@@ -4,15 +4,10 @@ function isTouchDevice() {
   if (typeof window === "undefined") return false;
   const prefixes = " -webkit- -moz- -o- -ms- ".split(" ");
 
+  // @ts-ignore
   function mq(query) {
     return typeof window !== "undefined" && window.matchMedia(query).matches;
   }
-  // @ts-ignore
-  if (
-    "ontouchstart" in window ||
-    (window?.DocumentTouch && document instanceof DocumentTouch)
-  )
-    return true;
   const query = ["(", prefixes.join("touch-enabled),("), "heartz", ")"].join(
     "",
   ); // include the 'heartz' - https://git.io/vznFH
