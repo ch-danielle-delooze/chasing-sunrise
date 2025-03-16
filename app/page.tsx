8959a0af2f2b28c8ada9d948f0c99e8a8701d64d
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-15 18:52:50 -0700

    use new font for navbar, change other usages to lg variant

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Chasing Sunrise</span>
+        <span className={title({ size: "lg" })}>Chasing Sunrise</span>
         <div className={subtitle({ class: "mt-4" })}>
           Photographs by Kash Desai for viewing and download.
         </div>
--- app/projects/[[...folder]]/page.tsx
+++ app/projects/[[...folder]]/page.tsx
@@ -19,7 +19,7 @@ export default function ImageFolderPage({ params }: ImageFolderPageProps) {
 
   return (
     <div className="w-full space-y-6">
-      <div className={title()}>{formatFolderName(folder)}</div>
+      <div className={title({ size: "lg" })}>{formatFolderName(folder)}</div>
       {subFolders?.map((subFolder) => (
         <ImageCarousel
           key={subFolder.name}
