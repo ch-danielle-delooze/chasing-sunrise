8bd2be340d2206bfc8a91fcf9f25df17fdf74749
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-15 17:59:10 -0700

    update font file and tweak title stylings

--- app/projects/[[...folder]]/page.tsx
+++ app/projects/[[...folder]]/page.tsx
@@ -19,7 +19,7 @@ export default function ImageFolderPage({ params }: ImageFolderPageProps) {
 
   return (
     <div className="w-full space-y-6">
-      <div className={title()}>{formatFolderName(folder).toUpperCase()}</div>
+      <div className={title()}>{formatFolderName(folder)}</div>
       {subFolders?.map((subFolder) => (
         <ImageCarousel
           key={subFolder.name}
