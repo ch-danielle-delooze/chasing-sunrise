8399e0816c8572d55c4e6e05894d0d8dcfeefdd8
Danielle DeLooze <118206403+ch-danielle-delooze@users.noreply.github.com>
2025-03-15 18:27:54 -0700

    21 fix mobile picture aspect ratio in image carousel (#24)
    * make image height dynamic based on device width
    
    * static width basis for mobile 2:3 aspect ratio
    
    * make 2/3 aspect ratio image basis dynamic based on container width
    
    * lint

--- components/imageCarousel/components/carouselImage.tsx
+++ components/imageCarousel/components/carouselImage.tsx
@@ -1,20 +1,41 @@
 "use client";
 import { Image } from "@heroui/image";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
-import { CarouselItem } from "../../ui/carousel";
-
-interface ImageProps {
+import { CarouselItem } from "@/components/ui/carousel";
+import useIsTouchDevice from "@/app/utils/device";
+interface CarouselImageProps {
   imagePath: string;
+  containerWidth: number;
 }
 
 const cloudFrontUrl = process.env.NEXT_PUBLIC_CHASING_SUNRISE_CLOUDFRONT_DOMAIN;
 
-const CarouselImage = ({ imagePath }: ImageProps) => {
+const CarouselImage = ({ imagePath, containerWidth }: CarouselImageProps) => {
   const [aspectRatio, setAspectRatio] = useState(0);
+  const isTouchDevice = useIsTouchDevice();
+
+  const getTouchDeviceHeight = () => {
+    if (!isTouchDevice) {
+      return 400;
+    } else {
+      return containerWidth / 1.5;
+    }
+  };
+  const touchDeviceHeight = useMemo(
+    () => getTouchDeviceHeight(),
+    [isTouchDevice],
+  );
+
+  const getTouchDeviceDynamicBasis = () => {
+    const imageWidth = touchDeviceHeight * (2 / 3);
+    const imageBasis = imageWidth / containerWidth;
+
+    return `basis-${imageBasis}`;
+  };
 
   const onImageLoad = (image: HTMLImageElement) => {
-    const aspectRatio = image.naturalHeight / image.naturalWidth;
+    const aspectRatio = image.naturalWidth / image.naturalHeight;
 
     setAspectRatio(aspectRatio);
   };
@@ -22,13 +43,13 @@ const CarouselImage = ({ imagePath }: ImageProps) => {
   return (
     <CarouselItem
       className={`
-      ${aspectRatio > 1 ? "md:basis-1/5 basis-2/3" : ""}
-      ${aspectRatio < 1 ? "md:basis-1/2" : ""}
+      ${aspectRatio < 1 ? `md:basis-1/5 ${getTouchDeviceDynamicBasis()}` : ""}
+      ${aspectRatio > 1 ? "md:basis-1/2" : ""}
     `}
     >
       <Image
         alt="Image"
-        height={400}
+        height={isTouchDevice ? touchDeviceHeight : 400}
         radius="none"
         src={`${cloudFrontUrl}${imagePath}`}
         onLoad={(image) => onImageLoad(image.currentTarget)}
--- components/imageCarousel/index.tsx
+++ components/imageCarousel/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useRef } from "react";
+
 import { subtitle } from "@/components/primitives";
 import { formatFolderName } from "@/app/utils/string";
 import CarouselImage from "@/components/imageCarousel/components/carouselImage";
@@ -16,14 +19,19 @@ interface ImageCarouselProps {
 
 const ImageCarousel = ({ imagePaths, folderName }: ImageCarouselProps) => {
   const isTouchDevice = useIsTouchDevice();
+  const containerRef = useRef<any>();
 
   return (
-    <div>
+    <div ref={containerRef}>
       <div className={subtitle()}>{formatFolderName(folderName)}</div>
       <Carousel>
         <CarouselContent>
           {imagePaths.map((imagePath) => (
-            <CarouselImage key={imagePath} imagePath={imagePath} />
+            <CarouselImage
+              key={imagePath}
+              containerWidth={containerRef?.current?.clientWidth}
+              imagePath={imagePath}
+            />
           ))}
         </CarouselContent>
         {/* If the device is a touch device, we don't want to show the previous and next buttons */}
