838cba2d6d7576279f9f9860089a16325b14ce97
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-03-15 17:16:11 -0700

    test font page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function AboutPage() {
   return (
     <div>
-      <h1 className={title()}>About</h1>
+      <h1 className={title()}>Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz ! " ; , . ?</h1>
     </div>
   );
 }
