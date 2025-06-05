7212e4a02773bd795d3e4a7ff2bba1eea1e05a67
Danielle DeLooze <danielle.delooze@charliehealth.com>
2025-06-05 16:30:25 -0700

    add link on about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,6 @@ import FancyParagraph from "@/components/fancyParagraph";
 export default function AboutPage() {
   const kashBioParagraph =
     "Welcome to our little corner of the internet! My passion for photography started when I was in medical school, finding every excuse to tinker with an old D-SLR camera and capture bits of life – from everyday views to scenic trips. I never imagined sharing my work on a platform like this, but thanks to my better half, Danielle, it’s been an absolute dream. I hope these projects bring inspiration to your day, and I will continue adding styles, locations, themes, and ideas to this along the way!\n\nCheers!\n\nKash Desai\n(very) Amateur Photographer,\nVascular Surgeon";
-  const ps =
-    "P.S. Content on this website is entirely free. Please credit @chasing_sunrise_photography if you use any content in any capacity. I’m happy to send you full-resolution images if you direct message me on Instagram. Any replies and correspondence will entirely depend on how busy my surgical schedule happens to be!";
   const dainsBioParagraph =
     "My passion for making a website to host all my boyfriend’s photographs started when he asked me to do it. Some persuasion and underpaid work aside, here we are! In the future, some of my work might make an appearance or two. Feel free to reach out for any website-related issues!\n\nWarmly,\n\nDanielle DeLooze (Software Engineer, Extraordinaire)";
 
@@ -14,7 +12,8 @@ export default function AboutPage() {
       <h1 className={title({ size: "lg" })}>About</h1>
 
       <FancyParagraph text={kashBioParagraph} />
-      <div className={fineprint()}>{ps}</div>
+      <div className={fineprint()}>P.S. Content on this website is entirely free. Please credit {<a href="https://www.instagram.com/chasing_sunrise_photography" target="_blank" rel="noopener noreferrer" className="underline text-blue-600">@chasing_sunrise_photography</a>}&nbsp;
+      if you use any content in any capacity. I’m happy to send you full-resolution images if you direct message me on Instagram. Any replies and correspondence will entirely depend on how busy my surgical schedule happens to be!</div>
 
       <FancyParagraph text={dainsBioParagraph} />
     </div>
